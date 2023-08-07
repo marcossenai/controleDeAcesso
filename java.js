af79d521300bc99c857f44ceb3417d02237090b9
marcossenai <133662787+marcossenai@users.noreply.github.com>
2023-08-07 19:24:03 -0300

    Adicionando Um java e inicio no CSS

---
+++ java.js
@@ -0,0 +1,18 @@
+function acesso() {
+    let numero = document.getElementById('cartao').value;
+    let mensagem = document.getElementById('acesso');
+  
+    if (numero >= 1000 && numero <= 1999) {
+      mensagem.textContent = 'Acesso total permitido.';
+      mensagem.style.color = 'green';
+    } else if (numero >= 2000 && numero <= 2999) {
+      mensagem.textContent = 'Acesso limitado permitido.';
+      mensagem.style.color = 'orange';
+    } else if (numero >= 3000) {
+      mensagem.textContent = 'Acesso negado.';
+      mensagem.style.color = 'red';
+    } else {
+      mensagem.textContent = 'Número de cartão inválido.';
+      mensagem.style.color = 'red';
+    }
+  }
